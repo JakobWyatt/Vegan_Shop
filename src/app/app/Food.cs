1bfb34878af228a7ef9850d205825e84c9e86adb
Jakob Wyatt <jakob.wyatt@gmail.com>
2019-02-08 12:01:47 +0800

    added Food container class

---
+++ src/app/app/Food.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Vegan_Shop
+{
+    public class Food
+    {
+        public String Name
+        {
+            get;
+        }
+
+        public Decimal Price
+        {
+            get;
+        }
+
+        public ImageSource Image
+        {
+            get;
+        }
+
+        public Food(String name, Decimal price, ImageSource image)
+        {
+            Name = name;
+            Price = price;
+            Image = image;
+        }
+    }
+}
