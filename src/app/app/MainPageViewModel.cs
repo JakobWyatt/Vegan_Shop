488d359d1a56a301345f587b5bb1f8bda70f9613
Jakob Wyatt <jakob.wyatt@gmail.com>
2019-02-08 12:03:29 +0800

    Implemented IMainPageViewModel

--- src/app/app/MainPageViewModel.cs
+++ src/app/app/MainPageViewModel.cs
@@ -1,31 +1,36 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace Vegan_Shop
 {
     //ViewModel implements IMainPageViewModel for view binding
     public class MainPageViewModel : IMainPageViewModel
     {
-        /*
-        private String _textBox;
-        public String TextBox
+        private IMainPageModel mainPageModel;
+
+        public MainPageViewModel( IMainPageModel inMainPageModel )
+        {
+            mainPageModel = inMainPageModel;
+        }
+
+        private ICommand _searchFood;
+        public ICommand SearchFood
         {
             get
             {
-                return _textBox;
-            }
-            set
-            {
-                if ( _textBox != value )
+                return _searchFood ?? (_searchFood = new Command( ( Object sender ) =>
                 {
-                    _textBox = value;
-                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("TextBox"));
-                }
+                    //Cannot reassign, as we would lose the binding to FoodList
+                    FoodList.Clear();
+                    mainPageModel.queryFood( sender.ToString() ).ForEach( FoodList.Add );
+                }));
             }
         }
-        */
-        public ObservableCollection<String> ItemList = new ObservableCollection<String>();
+
+        public ObservableCollection<Food> FoodList { get; } = new ObservableCollection<Food>();
 
         public event PropertyChangedEventHandler PropertyChanged;
     }
