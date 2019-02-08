352268e1cfa342ba1e7dc0f3573309350f08eb40
Jakob Wyatt <jakob.wyatt@gmail.com>
2019-02-08 12:04:54 +0800

    Added initial test for mainpage model

---
+++ src/app/app/Test/MainPageModelTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using Vegan_Shop;
+
+namespace Vegan_Shop_Test
+{
+    public class MainPageModelTest : IMainPageModel
+    {
+        public List<Food> queryFood( String query )
+        {
+            return TestData.TestFood.Where((Food food) =>
+            {
+                //Non-case sensitive search
+                return food.Name.ToLower().Contains( query.ToLower() );
+            }).ToList();
+        }
+    }
+}
