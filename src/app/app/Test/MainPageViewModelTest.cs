80d3058ffc63cd0556f744de09d34c7c48b846ef
Jakob Wyatt <jakob.wyatt@gmail.com>
2019-02-08 12:05:27 +0800

    added test to ensure items were working correctly in view

---
+++ src/app/app/Test/MainPageViewModelTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Vegan_Shop;
+
+namespace Vegan_Shop_Test
+{
+    public class MainPageViewModelTest : IMainPageViewModel
+    {
+        private ICommand _searchFood;
+        public ICommand SearchFood
+        {
+            get
+            {
+                return _searchFood ?? (_searchFood = new Command((Object sender) => {}));
+            }
+        }
+
+        public ObservableCollection<Food> FoodList { get; } = new ObservableCollection<Food>(TestData.TestFood);
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
