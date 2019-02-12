9dbad130e29a08ce36596c3e01c93bd125369748
Jakob Wyatt <jakob.wyatt@gmail.com>
2019-02-12 23:57:47 +0800

    added food id to TestData

--- src/app/app/Test/TestData.cs
+++ src/app/app/Test/TestData.cs
@@ -8,9 +8,9 @@ namespace Vegan_Shop_Test
     public static class TestData
     {
         public static readonly List<Food> TestFood = new List<Food>() {
-            new Food( "Cheese and Milk", 3, ImageSource.FromUri( new Uri("https://static.standard.co.uk/s3fs-public/thumbnails/image/2018/09/12/10/milkcheese1209a.jpg?w968") ) ),
-            new Food( "Cheese", 2, ImageSource.FromUri( new Uri("https://static1.squarespace.com/static/5a1592ff0abd04e470d48744/t/5a27bbeeec212d905273a14c/1512996237223/Kaas.jpeg?format=1500w") ) ),
-            new Food( "Milk", 1, ImageSource.FromUri( new Uri("https://img.taste.com.au/5VamxIAS/taste/2016/11/almond-milk-109280-1.jpeg") ) ),
+            new Food( 1, "Cheese and Milk", 3, ImageSource.FromUri( new Uri("https://static.standard.co.uk/s3fs-public/thumbnails/image/2018/09/12/10/milkcheese1209a.jpg?w968") ) ),
+            new Food( 2, "Cheese", 2, ImageSource.FromUri( new Uri("https://static1.squarespace.com/static/5a1592ff0abd04e470d48744/t/5a27bbeeec212d905273a14c/1512996237223/Kaas.jpeg?format=1500w") ) ),
+            new Food( 3, "Milk", 1, ImageSource.FromUri( new Uri("https://img.taste.com.au/5VamxIAS/taste/2016/11/almond-milk-109280-1.jpeg") ) ),
         };
     }
 }
