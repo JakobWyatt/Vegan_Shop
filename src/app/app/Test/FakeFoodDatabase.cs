bfa0485b3925f5df943e3008ac921b3d33569417
Jakob Wyatt <jakob.wyatt@gmail.com>
2019-02-12 23:57:30 +0800

    added non-database that implements database interface for testing

---
+++ src/app/app/Test/FakeFoodDatabase.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Vegan_Shop;
+
+namespace Vegan_Shop_Test
+{
+    //This class implements IFoodDatabase, but does not use a database at all
+    public class FakeFoodDatabase : IFoodDatabase
+    {
+        public void Update() { }
+
+        public Task<List<Food>> GetFoods()
+        {
+            return Task.FromResult(TestData.TestFood);
+        }
+    }
+}
