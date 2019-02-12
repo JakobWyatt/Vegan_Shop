0d1e29fb4a0ea9c3b41da3ef48e6b27e67801d77
Jakob Wyatt <jakob.wyatt@gmail.com>
2019-02-12 23:52:47 +0800

    added a data-link-layer interface

---
+++ src/app/app/IFoodDatabase.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Vegan_Shop
+{
+    public interface IFoodDatabase
+    {
+        Task<List<Food>> GetFoods();
+        void Update();
+    }
+}
