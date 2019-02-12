2fc7b18ff98d5379768a473511bebe8b11fa7502
Jakob Wyatt <jakob.wyatt@gmail.com>
2019-02-12 23:58:27 +0800

    added test of sqlite database

---
+++ src/app/app/Test/FoodDatabaseTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections.Generic;
+using Vegan_Shop;
+using SQLite;
+
+namespace Vegan_Shop_Test
+{
+    public class FoodDatabaseTest : IFoodDatabase
+    {
+        private readonly String dbPath;
+        private readonly SQLiteAsyncConnection connection;
+
+        public FoodDatabaseTest()
+        {
+            dbPath = Path.Combine(Environment.GetFolderPath(
+                Environment.SpecialFolder.LocalApplicationData), "FoodTestSQLite.db3");
+            connection = new SQLiteAsyncConnection(dbPath);
+        }
+
+        public async void Update()
+        {
+            //Fill with test data
+            await connection.DropTableAsync<Food>();
+            await connection.CreateTableAsync<Food>();
+            await connection.InsertAllAsync((IEnumerable<Food>)TestData.TestFood);
+        }
+
+        public async Task<List<Food>> GetFoods() => await connection.Table<Food>().ToListAsync();
+    }
+}
