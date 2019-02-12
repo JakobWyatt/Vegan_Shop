8d0f2a60d6c86aa54a2c291aca233ecd1dfb45e9
Jakob Wyatt <jakob.wyatt@gmail.com>
2019-02-12 23:58:12 +0800

    added synchronous test of sqlite database

---
+++ src/app/app/Test/FoodDatabaseTestSync.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Vegan_Shop;
+using SQLite;
+
+namespace Vegan_Shop_Test
+{
+    public class FoodDatabaseTestSync : IFoodDatabase
+    {
+        private readonly String dbPath;
+        private readonly SQLiteConnection connection;
+
+        public FoodDatabaseTestSync()
+        {
+            dbPath = Path.Combine(Environment.GetFolderPath(
+                Environment.SpecialFolder.LocalApplicationData), "FoodTestSyncSQLite.db3");
+            connection = new SQLiteConnection(dbPath);
+        }
+
+        public void Update()
+        {
+            //Fill with test data
+            connection.DropTable<Food>();
+            connection.CreateTable<Food>();
+            connection.InsertAll((IEnumerable<Food>)TestData.TestFood);
+        }
+
+        public Task<List<Food>> GetFoods() => Task.FromResult(connection.Table<Food>().ToList());
+    }
+}
