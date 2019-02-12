2701cbed3f4906b62fe70b25f4b8e19df92e355b
Jakob Wyatt <jakob.wyatt@gmail.com>
2019-02-12 23:57:07 +0800

    Made MainPageModelTest work with databases. Abstracted food filtering algorithm. Implemented async functionality

--- src/app/app/Test/MainPageModelTest.cs
+++ src/app/app/Test/MainPageModelTest.cs
@@ -1,16 +1,28 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
-using Xamarin.Forms;
+using System.Threading.Tasks;
 using Vegan_Shop;
 
 namespace Vegan_Shop_Test
 {
     public class MainPageModelTest : IMainPageModel
     {
-        public List<Food> queryFood( String query )
+        private IFoodDatabase FoodDatabase;
+
+        public MainPageModelTest( IFoodDatabase foodDatabase )
         {
-            return TestData.TestFood.Where((Food food) =>
+            FoodDatabase = foodDatabase;
+        }
+
+        public async Task<List<Food>> QueryFood( String query )
+        {
+            return FilterFood(await FoodDatabase.GetFoods(), query);
+        }
+
+        private List<Food> FilterFood( List<Food> foods, String query)
+        {
+            return foods.Where( (Food food) =>
             {
                 //Non-case sensitive search
                 return food.Name.ToLower().Contains( query.ToLower() );
