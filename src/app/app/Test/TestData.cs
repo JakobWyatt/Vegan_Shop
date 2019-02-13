adc310598bdf588f52a8fb604d2e94b37ceaf88e
Jakob Wyatt <jakob.wyatt@gmail.com>
2019-02-13 16:23:00 +0800

    updated testdata with new constructor

--- src/app/app/Test/TestData.cs
+++ src/app/app/Test/TestData.cs
@@ -8,9 +8,9 @@ namespace Vegan_Shop_Test
     public static class TestData
     {
         public static readonly List<Food> TestFood = new List<Food>() {
-            new Food( 1, "Cheese and Milk", 3, ImageSource.FromFile( "milk_and_cheese" ) ),
-            new Food( 2, "Cheese", 2, ImageSource.FromFile( "cheese" ) ),
-            new Food( 3, "Milk", 1, ImageSource.FromFile( "milk" ) )
+            new Food( 1, "Cheese and Milk", 3 ),
+            new Food( 2, "Cheese", 2 ),
+            new Food( 3, "Milk", 1 )
         };
     }
 }
