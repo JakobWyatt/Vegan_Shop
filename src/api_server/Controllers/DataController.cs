fea9e23b54391e076257bc063ab24234936a07d9
Jakob Wyatt <jakob.wyatt@gmail.com>
2019-02-13 20:22:15 +0800

    experimenting a bit with ASP.NET

---
+++ src/api_server/Controllers/DataController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api_server.Controllers
+{
+    //API ARCHITECTURE
+    //Under api/diff we have a page that returns the difference
+    //  between the given version and the current version
+    //Under api/data/{ID} we have the text based data for a product
+    //Under api/data/{ID}.png we have the image for a product
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public ActionResult<String> GetProductData(String id)
+        {
+            return id;
+        }
+    }
+}
---
+++ src/api_server/Controllers/DiffController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api_server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiffController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<String> GetDiff()
+        {
+            return "Diff functionality has not been implemented.";
+        }
+    }
+}
